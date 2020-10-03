1d298f2147d95934cb1eea6f66929fe2064d5ca7
iisydnii <61031342+iisydnii@users.noreply.github.com>
2020-10-03 01:17:50 -0400

    upload

---
+++ Lab4/Lab4/Sentence.cs
@@ -0,0 +1,110 @@
+﻿/////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+// Project: Lab 4
+// File Name: Sentence
+// Description: Creates a list of sentences for file Removes all punctionation and
+// gets a random sentence and Counts the words in a given sentences
+// Course: Server Side CSCI 2910-940
+// Author: Sydni Ward
+/////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text.RegularExpressions;
+
+namespace Lab4
+{
+    /// <summary>
+    /// Creates a list of sentences from a file Removes all punctionation and
+    /// gets a random sentence and Counts the words in a given sentences
+    /// </summary>
+    public class Sentence
+    {
+        public List<String> sentence;                       //List of orginial sentences from file
+        public List<String> sentenceWP;                     //List of manipulated sentences (Without punctionation and Uppercase)
+        Random rand = new Random();                         //Declaration of a random object
+        int count;                                          //Protected variable for method use for holding counts
+        String randOutput;                                  //Holds the random sentence generateds in RandomSentence()
+
+        /// <summary>
+        /// Constructor - used for building obects 
+        /// </summary>
+        public Sentence()
+        {
+            sentence = new List<String>();                   
+            sentenceWP = new List<String>();
+        }
+
+        /// <summary>
+        /// Creation of a list of sentences from a file
+        /// </summary>
+        /// <param name= "sentenceInput">String Input from a file</param>
+        /// <param name= "flag">flag to trigger end of file response</param>
+        public List<String> SentenceList(String sentenceInput, String flag)     //Makes List from file output, flag to trigger end of file response
+        {
+            if (flag != "X")
+            {
+                sentence.Add(sentenceInput);                // if no flag a sentence to orginial list
+            }
+            else
+            {                                               // once the flag is thrown 
+                count = sentence.Count();                   //Get count of number of sentences
+                if (count == 1)
+                {
+                    Console.WriteLine(sentence[0]);         //IF 1 Print for user
+                }
+                RemovePunctuation();                        //Call method to start text manipulation 
+                RandomSentence();                           //call method to get a random sentence
+            }
+            return sentence;
+        }
+
+        /// <summary>
+        /// gets a random sentence for sentence collection and Calls CountWords to count the words in that sentence
+        /// </summary>
+        public String RandomSentence()
+        {
+            randOutput = sentence[rand.Next(sentence.Count)];    //Outputs single Random sentence
+            CountWords(randOutput);                              //Call CountWords to get word count of random output
+            Words word1 = new Words(randOutput.ToUpper());                 ////take random output string and call Words class
+            return randOutput;                                   //Random return sentence
+        }
+
+        /// <summary>
+        /// Takes given string and counts it<
+        /// </summary>
+        /// <param name= "sentenceInput">Takes given string and counts it</param>
+        public int CountWords(String sentenceInput)
+        {
+            List<String> listStrLineElements;                   //Local list to hold words to later count
+            String delimitor = " ";                             //Setting delimiter this is what tells the program where to split the string
+            count = 0;                                          // Count is 0 
+
+            listStrLineElements = sentenceInput.Split(delimitor).ToList();      //Takes input of a given string and creates a list of words 
+            count = listStrLineElements.Count();                                //Get and set count
+
+            return count;                                                       //return count
+        }
+
+        /// <summary>
+        /// Removes Punctuation using Regex and Makes sentences Uppercase then adds it to it's own list sentenceWP
+        /// </summary>
+        public List<String> RemovePunctuation()                                 //Removing Punctuation and creating a list 
+        {
+            String stripped;
+            System.Text.RegularExpressions.Regex TitleRegex = new
+            System.Text.RegularExpressions.Regex(@"[^\sa-z0-9]+",               //Get rid of all chars that are not spaces,letter,or numbers
+            System.Text.RegularExpressions.RegexOptions.IgnoreCase);            //ingnore casing
+
+            foreach (String se in sentence)                                     //loop through sentence collection
+            {
+                stripped = TitleRegex.Replace(se, String.Empty);                //replace punctuation with empty string
+
+                stripped = stripped.ToUpper();                                  //Make Uppercase
+                sentenceWP.Add(stripped);                                       //Add to sentenceWP collection
+            }
+            Words word1 = new Words(sentenceWP);                                //take sentenceWP and call Words class
+            return sentenceWP;                                                  // return sentenceWP collection
+        }
+    }
+}
+
---
+++ Lab4/Lab4/Words.cs
@@ -0,0 +1,85 @@
+﻿/////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+// Project: Lab 4
+// File Name: Words
+// Description: Creates a list of words and keeps count
+// Course: Server Side CSCI 2910-940
+// Author: Sydni Ward
+/////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Lab4
+{
+    /// <summary>
+    /// Creates a list of words and keeps count
+    /// </summary>
+    public class Words
+    {
+        List<String> words;                 //collection of words used in sentences
+        Dictionary<String, int> wordCount; //collection of words to get word count to keep dictionary of every word used
+        Word word2;                         //declare object
+
+        /// <summary>
+        /// Constructor Takes in a list of sentences calls ListOfWords
+        /// Calls word call using sentences collection
+        /// </summary>
+        /// <param name= "sentences"> Takes in a list of sentences </param>
+        public Words(List<String> sentences) //Takes in a list of sentences, Example of overloading
+        {
+            wordCount = new Dictionary<string, int>();
+            words = new List<string>();
+            word2 = new Word();                //initialize object
+            foreach (string se in sentences)
+            {
+                ListOfWords(se);                    //Takes single string and calls ListOfWords method
+            }
+            word2.AddingUpChars(sentences);         //Calls word call using sentences collection
+        }
+
+        /// <summary>
+        /// Constructor Takes in a single string sentence calls ListOfWords
+        /// Calls word call using sentences collection
+        /// </summary>
+        /// <param name= "sentences"> Takes in a single string sentence </param>
+        public Words(String sentence)               //Example of overloading
+        {
+            wordCount = new Dictionary<string, int>();
+            words = new List<string>();
+            word2 = new Word();                     //initialize object
+            ListOfWords(sentence);                  //Takes single string and calls ListOfWords method
+            word2.AddingUpChars(sentence);
+        }
+
+        /// <summary>
+        /// Takes in a single string returns list of words of the given string
+        /// </summary>
+        /// <param name= "input">Takes in a single string</param>
+        public List<String> ListOfWords(String input)   //Takes in a single string returns list of words of the given string
+        {
+            String delimitor = " ";                     //Sets local delimitor as a space 
+            words = input.Split(delimitor).ToList();    //splits the string by the local delimitor    
+            return words;                               //returns list of word of a given string
+        }
+
+        /// <summary>
+        /// CountWords - loop through collection of words to get word count to keep dictionary of every word used
+        /// </summary>
+        public void CountWords()
+        {
+            int value;                                  //local declaration of value to temporarily hold value from Dictionary
+            foreach(String word in words)               //loop through collection of words to get word count
+            { 
+                if (wordCount.ContainsKey(word))        //if that word is in the dictionary
+                {
+                    value = wordCount[word];           //get current value
+                    wordCount[word] = value + 1;       //update value in dictionary by adding 1
+                }
+                else
+                {
+                    wordCount.Add(word, 1);             //adding a new word as key and value as 1
+                }    
+            }
+        }
+    }
+}
